74dc1cf0dfff4d1a2aac22c9c590467e55e186c9
dennougorilla <dennougorilla@gmail.com>
2018-12-15 20:27:24 +0900

    add material design pwa

---
+++ zugo/nuxt.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'zugo',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '//fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,700,400italic|Material+Icons' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#448aff' },
+  /*
+  ** Build configuration
+  */
+  css: [
+    { src: 'vue-material/dist/vue-material.min.css', lang: 'css' },
+    { src: '~/assets/theme.scss', lang: 'scss' } // include vue-material theme engine
+  ],
+  modules: [
+      '@nuxtjs/pwa',
+  ],
+  plugins: [
+    { src: '~/plugins/vue-material' }
+  ],
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    vendor: ['vue-material'],
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ zugo/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="page-container">
+    <md-app md-waterfall md-mode="fixed">
+      <md-app-toolbar class="md-primary">
+        <span class="md-title">ZUGO</span>
+      </md-app-toolbar>
+
+      <md-app-content>
+      </md-app-content>
+    </md-app>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .md-app {
+    min-height: 100vh;
+    border: 1px solid rgba(#000, .12);
+  }
+</style>
+
+<script>
+export default {
+  name: 'Waterfall'
+}
+</script>
